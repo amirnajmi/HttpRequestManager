d643a855f1aadbba4bac5fb019ec53627d9dd49c
AmirHossein Najmi <amirhossein@paisley.digital>
2020-07-17 13:06:50 +0200

    Refactor executeMethod to accept two parameters(headers, body), JSESSIONID was removed and cookie management is done automatically.

--- src/main/HttpRequestManager.java
+++ src/main/HttpRequestManager.java
@@ -2,6 +2,8 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,57 +17,53 @@ import java.util.stream.Collectors;
 public class HttpRequestManager {
 
     private static final String REQUEST_DELIMITER = "------------------------------------------------------------------";
-    private String jSessionID;
+    private HttpClient httpClient;
 
     public static void main(String[] args) {
-        String result = new HttpRequestManager().executeCommand();
+        List<String> strings = new HttpRequestManager().parseRequestFile();
+        String firstReq = strings.get(0);
+        List<String> collect = firstReq.lines().collect(Collectors.toList());
+        String body = collect.get(collect.size() - 1);
+        collect.remove(body);
+        StringBuilder stringBuilder = new StringBuilder();
+        collect.forEach(s -> {
+            stringBuilder.append(s).append(System.lineSeparator());
+        });
+        String result = null;
+        try {
+            result = new HttpRequestManager().executeCommand(stringBuilder.toString(), body);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println(result);
     }
 
-    public String executeCommand() {
-        List<String> requestContents = parseRequestFile();
-        StringBuilder responseBody = new StringBuilder();
-        List<String> responses = requestContents.stream().map(this::sendRequest).collect(Collectors.toList());
-        System.out.println(responses);
-        return responseBody.toString();
+    public String executeCommand(String headerContent, String body) throws IOException, InterruptedException {
+        CookieHandler.setDefault(new CookieManager());
+        this.httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .connectTimeout(Duration.ofSeconds(40))
+                .cookieHandler(CookieHandler.getDefault()).build();
+        return sendRequest(headerContent, body);
+
     }
 
-    private String sendRequest(String requestContent) {
-        HttpRequest req = generateRequest(requestContent);
-        try {
-            HttpClient httpClient = HttpClient.newBuilder()
-                    .version(HttpClient.Version.HTTP_1_1)
-                    .connectTimeout(Duration.ofSeconds(40)).build();
-            HttpResponse<String> res = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
-            Map<String, List<String>> headerMap = res.headers().map();
-            if (headerMap.containsKey("set-cookie")&& jSessionID==null) {
-                String cookie = headerMap.get("set-cookie").get(0);
-                String[] split = cookie.split(";");
-                jSessionID = cookie;
-            }
-            return res.body();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "";
+    private String sendRequest(String headersContent, String body) throws IOException, InterruptedException {
+
+        HttpRequest req = generateRequest(headersContent, body);
+        HttpResponse<String> res = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+        return res.body();
     }
 
-    private HttpRequest generateRequest(String requestContent) {
+    private HttpRequest generateRequest(String headersContent, String body) {
         HttpRequest httpRequest = null;
-        List<String> lines = requestContent.lines()
+        List<String> lines = headersContent.lines()
                 .filter(line -> !line.isEmpty() && !line.isBlank()).collect(Collectors.toList());
         String firstLine = lines.get(0);
         lines.remove(firstLine);
         String[] requestCommand = firstLine.split(" ");
         String httpMethod = requestCommand[0];
         String uri = requestCommand[1];
-        String body = null;
-        if (httpMethod.equalsIgnoreCase("POST")) {
-            body = lines.get(lines.size() - 1);
-            lines.remove(body);
-        }
         Map<String, String> headers = new HashMap<>();
         lines.forEach(string -> string.lines().forEach(line -> {
             String[] header = line.split(":");
@@ -87,14 +85,7 @@ public class HttpRequestManager {
                         .uri(URI.create(uri)).build();
                 break;
         }
-        if (headers.containsKey("Cookie")) {
-            headers.remove("Cookie");
-        }
         headers.forEach(requestBuilder::header);
-        if (jSessionID != null) {
-            String[] split = jSessionID.split("=");
-            requestBuilder.header("Cookie", jSessionID);
-        }
         return requestBuilder.build();
     }
 
