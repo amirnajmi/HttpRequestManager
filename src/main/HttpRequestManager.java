870f4ad7272de80aed7b5f6088a4e1e554f9d2d8
AmirHossein Najmi <amirhossein@paisley.digital>
2020-07-17 16:00:46 +0200

    parse test file in main method to test refactored code

--- src/main/HttpRequestManager.java
+++ src/main/HttpRequestManager.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 public class HttpRequestManager {
